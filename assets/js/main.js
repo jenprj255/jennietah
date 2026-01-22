7e931c779cc254527e70094fa886a3d8bad7a433
Jennie Tahardi <jenavigatinglife@gmail.com>
2026-01-22 21:52:53 +0800

    updated webcv

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,5 +91,68 @@ $(function () {
         }, 1500);
     });
 
+    //===== WeChat QR toggle (tap/click to mimic hover)
+    $('.wechat_wrapper > a').on('click', function (event) {
+        var href = $(this).attr('href') || '';
+        if (href !== '#' && href !== 'javascript:void(0)') {
+            return;
+        }
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        var $wrapper = $(this).closest('.wechat_wrapper');
+        $wrapper.toggleClass('is-open');
+        $wrapper.siblings('.wechat_wrapper').removeClass('is-open');
+    });
+
+    $('.wechat_wrapper').on('mouseenter', function () {
+        $(this).addClass('is-open');
+    }).on('mouseleave', function () {
+        $(this).removeClass('is-open');
+    });
+
+    $(document).on('click', function () {
+        $('.wechat_wrapper').removeClass('is-open');
+    });
+
+    $('.wechat_wrapper .wechat_qr').on('click', function (event) {
+        event.stopPropagation();
+    });
+
+    //===== Experience photo lightbox
+    $(document).on('click', '.detail_gallery img', function () {
+        var src = $(this).attr('src');
+        var alt = $(this).attr('alt') || 'Photo';
+
+        var $overlay = $('<div class="lightbox_overlay" role="dialog" aria-modal="true"></div>');
+        var $img = $('<img>', { src: src, alt: alt });
+        var $close = $('<button class="lightbox_close" type="button" aria-label="Close">×</button>');
+
+        $overlay.append($img, $close);
+        $('body').append($overlay);
+
+        $close.on('click', function () {
+            $overlay.remove();
+        });
+
+        $overlay.on('click', function (event) {
+            if (event.target === $overlay[0]) {
+                $overlay.remove();
+            }
+        });
+
+        $img.on('click', function () {
+            $(this).toggleClass('is-zoomed');
+        });
+
+        $(document).on('keydown.lightbox', function (event) {
+            if (event.key === 'Escape') {
+                $overlay.remove();
+                $(document).off('keydown.lightbox');
+            }
+        });
+    });
+
 
 });
